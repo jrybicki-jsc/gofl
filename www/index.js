abf8328e65c4486a0cf4099759f0f1a983054d48
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-10-13 09:10:58 +0200

    fps

--- www/index.js
+++ www/index.js
@@ -17,15 +17,100 @@ canvas.width = (CELL_SIZE +1) * width +1;
 
 const ctx = canvas.getContext('2d');
 
+let animationId = null;
+
+const fps = new class {
+     constructor() {
+        this.fps = document.getElementById("fps");
+        this.frames = []
+        this.lastFrameTimeStamp = performance.now();
+     }
+
+    render() {
+        const now = performance.now();
+        const delta = now - this.lastFrameTimeStamp;
+        this.lastFrameTimeStamp = now;
+        const  fps = 1/ delta * 1000;
+       
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift();
+        }
+
+        let min = Infinity;
+        let max = -Infinity;
+    
+        let sum = 0;
+        for (let i = 0; i < this.frames.length; i++) {
+            sum += this.frames[i];
+            min = Math.min(this.frames[i], min);
+            max = Math.max(this.frames[i], max);
+        }
+        let mean = sum /this.frames.length;
+
+        this.fps.textContent = `
+Frames per Second:
+        latest = ${Math.round(fps)}
+         mean  = ${Math.round(mean)}
+          min  = ${Math.round(min)}
+          max =  ${Math.round(max)}
+`.trim();
+    }
+};
 
 const renderLoop = () => {
+    fps.render();
     universe.tick();
    
     drawGrid();
     drawCells();
 
-    requestAnimationFrame(renderLoop);
+    animationId = requestAnimationFrame(renderLoop);
 };
+
+const isPaused = () => {
+    return animationId === null;
+};
+
+const playPauseButton = document.getElementById("play-pause");
+
+const play = () => {
+   playPauseButton.textContent = "||";
+   renderLoop();
+};
+
+const pause = () => {
+    playPauseButton.textContent = "▶";
+    cancelAnimationFrame(animationId);
+    animationId = null;
+};
+
+playPauseButton.addEventListener("click", event => {
+    if (isPaused()) {
+        play();
+    } else {
+        pause();
+    }
+});
+
+
+canvas.addEventListener("click", event => {
+    const bR = canvas.getBoundingClientRect();
+    
+    const scaleX = canvas.width / bR.width;
+    const scaleY = canvas.height / bR.height;
+
+    const canvasLeft = (event.clientX - bR.left) * scaleX;
+    const canvasTop = (event.clientY - bR.top) * scaleY;
+
+    const row = Math.min(Math.floor(canvasTop / (CELL_SIZE +1)), height -1);
+    const col = Math.min(Math.floor(canvasLeft/ (CELL_SIZE +1)), width -1 );
+    universe.toggle_cell(row, col);
+
+    drawGrid();
+    drawCells();
+
+});
 
 
 const drawGrid = () => {
@@ -75,4 +160,5 @@ const drawCells = () => {
 
 drawGrid();
 drawCells();
-requestAnimationFrame(renderLoop);
+
+play();
